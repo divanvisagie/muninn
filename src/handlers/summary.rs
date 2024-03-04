a76fcf2258bbb67be9b3c9ceba1559d0532930cb
Divan Visagie <visagiedivan@gmail.com>
2024-03-04 21:39:59 +0100

    Move out summary service

--- src/handlers/summary.rs
+++ src/handlers/summary.rs
@@ -1,49 +1,7 @@
 use actix_web::{web, HttpResponse};
-use chrono::NaiveDate;
 use tracing::error;
-use std::sync::Arc;
-use tokio::sync::Mutex; 
 
-use crate::{repos::messages::MessageRepo, Resources};
-
-pub struct SummaryService {
-    pub message_repo: Arc<Mutex<dyn MessageRepo>>,
-    pub embedding_client: Arc<Mutex<dyn crate::clients::embeddings::EmbeddingsClient>>,
-}
-
-impl SummaryService {
-    pub async fn summarize_chats_for_user_for_date(
-        &self,
-        user: String,
-        date_str: String,
-    ) -> Result<Vec<String>, ()> {
-        let date = match NaiveDate::parse_from_str(&date_str, "%Y-%m-%d") {
-            Ok(date) => date,
-            Err(_) => {
-                return Err(());
-            }
-        };
-
-        let messages = self
-            .message_repo
-            .lock()
-            .await
-            .get_all_for_user_on_day(user.clone(), date);
-
-        match messages {
-            Ok(messages) => {
-                let mut summaries = vec![];
-                for message in messages {
-                    summaries.push(message.content.clone());
-                }
-                return Ok(summaries);
-            }
-            Err(_) => {
-                return Err(());
-            }
-        }
-    }
-}
+use crate::{services::summary::SummaryService, Resources};
 
 pub async fn get_summary(
     resources: web::Data<Resources>,
